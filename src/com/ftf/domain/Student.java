fc282eecc11305f9ac06410f603daa8f9cb51509
AryaGitHub1 <1963708638@qq.com>
2020-09-01 13:34:42 +0800

    提交测试文件

---
+++ src/com/ftf/domain/Student.java
@@ -0,0 +1,15 @@
+package com.ftf.domain;
+
+import java.net.Inet4Address;
+
+public class Student {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
